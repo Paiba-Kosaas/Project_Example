0a3e977e61a6cdb139be32899ac6b86b3eb1d3b0
Jorge <jorge.simancas@eurotransportcar.com>
2023-11-03 09:54:32 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("form").submit(function(event){
+        event.preventDefault();
+        var name = $("#mail-name").val();
+        var email = $("#mail-email").val();
+        var gender = $("#mail-gender").val();
+        var message = $("#mail-message").val();
+        var submit = $("#mail-submit").val();
+        $(".form-message").load("mail.php", {
+          name: name,
+          email: email,
+          gender: gender,
+          message: message,
+          submit: submit
+        });
+    });
+});
